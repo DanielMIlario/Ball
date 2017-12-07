5af80ec8891e1128e16a2e6c3340621e5b398036
Daniel <Daniel.m.ilario@gmail.com>
2017-12-07 15:19:07 -0800

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var x = 0;
+var speed = 5;
+
+function setup () {
+  createCanvas(2000, 1000)
+}
+
+function draw() {
+  background(50, 100, 50)
+  stroke(51)
+  strokeWeight(6);
+  fill(0, 0, 255);
+  ellipse(x, 500, 100, 100);
+
+  if (x > width) {
+    speed = -5;
+  }
+  x = x + speed;
+
+
+
+
+
+
+
+
+}
