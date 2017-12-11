8aadb3afea81498a4e970daceb28c06c576d4d06
Daniel <Daniel.m.ilario@gmail.com>
2017-12-10 18:39:34 -0800

    Hovering the rect draws an ellipse

--- sketch.js
+++ sketch.js
@@ -4,47 +4,57 @@ var speed = {
   x:5,
   y:5,
 }
-
-var rectWidth = 20
-var rectHeight = 150
+var value = 0
 
 function setup () {
   createCanvas(2000, 1000)
 }
-
-function draw() {
-  background(50, 100, 50)
-  stroke(51)
-  strokeWeight(6);
-  fill(0, 0, 255);
-  ellipse(x, y, 100, 100);
-  rect(100, mouseY - 75, 20, 150)
-
-  if(x + 50 == mouseY) {
-    speed.x = 15
-  }
-  if (x > (width - 50)) {
+function isInWidth(ballX) {
+  if (ballX > (width - 50)) {
     speed.x = -5;
-  } else if (x < 0) {
+  } else if (ballX < 0) {
     speed.x = 5;
   }
-  if (y > (height - 50)) {
+}
+
+function isInHeight(ballY) {
+  if (ballY > (height - 50)) {
     speed.y = -5;
-  } else if (y < 0) {
+  } else if (ballY < 0) {
     speed.y = 5;
   }
+}
+
+function draw() {
+  noStroke();
+  background(50, 100, 50)
+  fill(0, 0, 255);
+  // ellipse(mouseX, mouseY, 100, 100);
+  rect(200, 300, 20, 150);
+  var isInY = (mouseY > 300) && (mouseY < 450)
+  var isInX = (mouseX > 200) && (mouseX < 220)
+  // is in the frame
+  // isInWidth(x);
+  // isInHeight(y);
+if (isInX && isInY) {
+  ellipse(width / 2, height / 2, 20, 20)
+}
+
+
+  // Has collided with the paddle
+
   y = y + speed.y;
   x = x + speed.x;
-  }
+}
 
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
-  speed.x = speed.x - 1;
-} else if (keyCode === RIGHT_ARROW) {
-  speed.x = speed.x + 1;
-} if (keyCode === DOWN_ARROW) {
-  speed.y = speed.y - 1;
-} else if (keyCode === UP_ARROW) {
-  speed.y = speed.y + 1;
-}
+    speed.x = speed.x - 1;
+  } else if (keyCode === RIGHT_ARROW) {
+    speed.x = speed.x + 1;
+  } if (keyCode === DOWN_ARROW) {
+    speed.y = speed.y - 1;
+  } else if (keyCode === UP_ARROW) {
+    speed.y = speed.y + 1;
+  }
 }
