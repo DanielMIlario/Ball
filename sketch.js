312e3265a78f9f6ae0727638b766155dd284a5e2
Daniel <Daniel.m.ilario@gmail.com>
2017-12-12 16:12:57 -0800

    2 Balls interact with paddles

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
 var x = 0;
 var y = 0;
-var x2 = 0.25;
-var y2 = 0.25;
+var w = 1800;
+var z = 100;
+var height = 1000
+var width = 2000
 var speed = {
   x:5,
   y:5,
+}
+var speed2 = {
+  w:7,
+  z:7,
 }
 var value = 0
 
@@ -27,49 +33,45 @@ function isInHeight(ballY) {
   }
 }
 
-function isInWidth2(ballX2) {
-  if (ballX2 > (width - 50)) {
-    speed.x = -5;
-  } else if (ballX2 < 0) {
-    speed.x = 5;
-  }
-}
-
-function isInHeight2(ballY2) {
-  if (ballY2 > (height - 50)) {
-    speed.y = -5;
-  } else if (ballY2 < 0) {
-    speed.y = 5;
-  }
-}
-
 function draw() {
   noStroke();
   background(50, 100, 50)
   fill(0, 0, 255);
-  ellipse(x, y, 50, 50);
-  fill(0, 0, 255);
-  noStroke();
-  ellipse(x2, y2, 150, 150);
+  ellipse(x, y, 100, 100);
+  fill(255, 0, 0);
+  ellipse(w, z, 150, 150);
+  fill(0, 255, 0);
   rect(200, mouseY, 20, 150);
   var isInY = (y > mouseY) && (y < (mouseY + 150))
   var isInX = (x > 170) && (x < 250)
+  var isInZ = (z > mouseY) && (z < (mouseY + 150))
+  var isInW = (w > 170) && (w < 250)
   // is in the frame
   isInWidth(x);
   isInHeight(y);
-  isInWidth2(x2);
-  isInHeight2(y2);
 
+  if (z > (height - 50)) {
+    speed2.z = -7;
+  } else if (z < 0) {
+    speed2.z = 7;
+  }
+
+  if (w > (width - 50)) {
+    speed2.w = -7;
+  } else if (w < 0) {
+    speed2.w = 7;
+  }
 
 if (isInX && isInY) {
   speed.x = -speed.x;
 }
+if (isInZ && isInW) {
+  speed2.w = -speed2.w;
+}
   y = y + speed.y;
   x = x + speed.x;
-
-  y2 = y2 + speed.y + 1;
-  x2 = x2 + speed.x + 1;
-
+  z = z + speed2.z;
+  w = w + speed2.w;
   // Has collided with the paddle
 }
 
