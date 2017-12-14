f8695cf6fd6ac1c11f796914225629829b32b0e7
Christian Kosman <christiankosman@gmail.com>
2017-12-13 17:53:53 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,20 +6,20 @@ var B1x = 300;
 var B1y = 300;
 var B2x = 300;
 var B2y = 300;
-var height = 1000
-var width = 2000
+var height = 1000;
+var width = 2000;
 var speed = {
   x:5,
   y:5,
-}
+};
 var speed2 = {
   w:7,
   z:7,
-}
-var value = 0
+};
+var value = 0;
 
 function setup () {
-  createCanvas(2000, 1000)
+  createCanvas(2000, 1000);
 }
 function isInWidth(ballX) {
   if (ballX > (width - 50)) {
@@ -54,10 +54,10 @@ function draw() {
   //Paddle
   fill(0, 255, 0);
   rect(200, mouseY, 20, 150);
-  var isInX = (x + B1x > 130) && (x < B1x + 100);
+  var isInX = (x > 120) && (x < B1x + 100);
   var isInY = (y > mouseY) && (y < (mouseY + 150));
   var isInZ = (z > mouseY) && (z < (mouseY + 150));
-  var isInW = (w > 130) && (w < B2x + 100);
+  var isInW = (w > 120) && (w < B2x + 100);
   var B1g = (B1x <= 0) && (B1y <= 0);
   var B2g = (B2x <= 0) && (B2y <= 0);
   // is in the frame
@@ -85,14 +85,14 @@ function draw() {
 if (isInX && isInY) {
   speed.x = -speed.x;
   speed.y = -speed.y;
-  B1x = B1x * 0.7;
-  B1y = B1y * 0.7;
+  B1x *= 0.7;
+  B1y *= 0.7;
 }
 if (isInZ && isInW) {
   speed2.w = -speed2.w;
   speed2.z = -speed2.z;
-  B2x = B2x * 0.7;
-  B2y = B2y * 0.7;
+  B2x *= 0.7;
+  B2y *= 0.7;
 }
 if (B1g && B2g) {
   textSize(125);
@@ -103,21 +103,21 @@ if (B1g && B2g) {
   speed2.w = 0;
   speed2.z = 0;
 }
-  y = y + speed.y;
-  x = x + speed.x;
-  z = z + speed2.z;
-  w = w + speed2.w;
+  y += speed.y;
+  x += speed.x;
+  z += speed2.z;
+  w += speed2.w;
   // Has collided with the paddle
 }
 
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
-    speed.x = speed.x - 1;
+    speed.x -= - 1;
   } else if (keyCode === RIGHT_ARROW) {
-    speed.x = speed.x + 1;
+    speed.x += 1;
   } if (keyCode === DOWN_ARROW) {
-    speed.y = speed.y - 1;
+    speed.y -= 1;
   } else if (keyCode === UP_ARROW) {
-    speed.y = speed.y + 1;
+    speed.y += 1;
   }
 }
